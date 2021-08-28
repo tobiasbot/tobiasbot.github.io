cdbaeacdf2cc6be6cbeef14c1dc18406395d7f41
CheryX <roxmb25@gmail.com>
2021-08-28 23:57:57 +0200

    Update 2
    - Switched from `fontawesome` to `google fonts`
    - Deleted .otf font files
    - Added `nav` menu
    - Fixed resolution bugs
    - Added left article page

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function hide() {
     document.getElementById("footer").style.display = "none";
 }
+function toggledrop() {
+    var x = document.getElementById("dropdown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
